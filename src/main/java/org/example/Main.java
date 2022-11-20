909748d158d7add48494c118045b444e154207d4
xtz <xtz@gugu.com>
2022-11-20 13:28:32 +0800

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("hello,git2");
         System.out.println("hello,git3");
+        System.out.println("test");
 
     }
 }
