c13e40e2acb74ff8209244b9d2026c7258d2f5a5
lloopexpl <110380298+lloopexpl@users.noreply.github.com>
2022-11-20 13:31:55 +0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("hello,git2");
         System.out.println("hello,git3");
         System.out.println("test");
+        System.out.println("qwewqew");
 
     }
 }
